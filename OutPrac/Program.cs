cc74b5c47d87bfeb8c06402531fa11ce1fc28071
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-06-30 13:55:03 +0900

    초기 커밋.

---
+++ OutPrac/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("사용자 입력 : ");
+            int output;
+            bool reault = int.TryParse(
+                Console.ReadLine(),
+                out output);
+            if (reault)
+            {
+                Console.WriteLine("값 : " + output);
+            }
+            else
+            {
+                Console.WriteLine("정수를 입력하세요");
+            }
+        }
+    }
+}
